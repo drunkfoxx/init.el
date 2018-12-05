c131b08c14722297aed2ced1ccca3f5dd158ea8f
drunkfoxx <30527742+drunkfoxx@users.noreply.github.com>
2018-12-05 21:51:55 +0300

    Update init.el

--- init.el
+++ init.el
@@ -2,7 +2,8 @@
 (setq package-enable-at-startup nil)
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
- ("melpa" . "https://melpa.milkbox.net/packages/"))
+	("melpa" . "https://melpa.milkbox.net/packages/")
+	("org" . "https://orgmode.org/elpa/"))
       package-archive-priorities
       '(("melpa" . 20)
  ("gnu" . 15)))
@@ -14,9 +15,9 @@
 
 (eval-when-compile
   (require 'use-package))
-(require 'bind-key)
 
-(setq show-paren-mode t)
+(require 'bind-key)
+(show-paren-mode 1)
 (setq slime-lisp-implementations '((sbcl ("/usr/bin/sbcl"))))
 (require 'slime)
 (autoload 'slime-highlight-edits-mode "slime-highlight-edits" nil t)
@@ -30,11 +31,13 @@
 (add-to-list 'auto-mode-alist '("/etc/sbclrc$" . lisp-mode))
 (ido-mode 1)
 (setq dired-dwim-target t)
+
 (use-package expand-region
   :ensure t
   :bind
   (("C-." . 'er/expand-region)
    ("C->" . 'er/contract-region)))
+
 (use-package elisp-slime-nav
   :ensure t
   :config
@@ -43,6 +46,7 @@
     (mapcar
 	 (lambda (hook) (add-hook hook 'elisp-slime-nav-mode))
 	 '(emacs-lisp-mode-hook ielm-mode-hook))))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -52,8 +56,19 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes (quote (whiteboard)))
- '(package-selected-packages (quote (paredit helm pdf-tools slime slime-company)))
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(global-highlight-parentheses-mode t)
+ '(hl-paren-colors
+   (quote
+    ("green" "medium spring green" "green yellow" "medium aquamarine")))
+ '(initial-buffer-choice "~/common-lisp")
+ '(org-babel-load-languages (quote ((lisp . t) (emacs-lisp . t))))
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+ '(package-selected-packages
+   (quote
+    (org hydra paredit helm pdf-tools slime slime-company)))
  '(pdf-tools-enabled-modes
    (quote
     (pdf-history-minor-mode pdf-isearch-minor-mode pdf-links-minor-mode pdf-misc-minor-mode pdf-outline-minor-mode pdf-misc-size-indication-minor-mode pdf-misc-menu-bar-minor-mode pdf-annot-minor-mode pdf-sync-minor-mode pdf-misc-context-menu-minor-mode pdf-cache-prefetch-minor-mode pdf-occur-global-minor-mode)))
@@ -61,7 +76,9 @@
  '(pdf-view-midnight-colors (quote ("white" . "gray18")))
  '(slime-company-after-completion nil)
  '(slime-company-completion (quote simple))
- '(slime-truncate-lines t))
+ '(slime-truncate-lines t)
+ '(wdired-allow-to-change-permissions (quote advanced)))
+
 ;; helm config from Sacha Chua
 (use-package helm
   :ensure t
@@ -72,8 +89,8 @@
     (setq helm-candidate-number-limit 100)
     ;; From https://gist.github.com/antifuchs/9238468
     (setq helm-idle-delay 0.0 ; update fast sources immediately (doesn't).
-	  helm-input-idle-delay 0.01	; this actually updates things
-                                        ; reeeelatively quickly.
+	  helm-input-idle-delay 0.01  ; this actually updates things
+                                      ; reeeelatively quickly.
 	  helm-yas-display-key-on-candidate t
 	  helm-quick-update t
 	  helm-M-x-requires-pattern nil
@@ -95,7 +112,6 @@
 	 ("C-x c Y" . helm-yas-create-snippet-on-region)
 	 ("C-x c SPC" . helm-all-mark-rings)))
 
-;;paredit config
 (use-package paredit
   :ensure t
   :config
@@ -106,6 +122,37 @@
     (add-hook 'ielm-mode-hook #'enable-paredit-mode)
     (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
     (add-hook 'slime-repl-mode-hook #'enable-paredit-mode)))
+
+(use-package hydra
+  :ensure t
+  :config
+  (defhydra hydra-paredit-home-row (global-map "C-c p")
+    "qwerty joystick style"
+    ("j" paredit-backward "left")
+    ("l" paredit-forward "right")
+    ("i" paredit-backward-up "up")
+    ("k" paredit-forward-down "down")
+    ("u" paredit-backward-down "prev")
+    ("o" paredit-forward-up "next")
+    ("(" paredit-wrap-round)
+    ("C-t" transpose-sexps)
+    ("0" universal-argument)
+    ("1" digit-argument)
+    ("2" digit-argument)
+    ("3" digit-argument)
+    ("4" digit-argument)
+    ("5" digit-argument)  
+    ("6" digit-argument)
+    ("7" digit-argument)
+    ("8" digit-argument)
+    ("9" digit-argument)
+    (";" er/expand-region) 
+    (":" er/contract-region)))
+
+(setq org-confirm-babel-evaluate nil
+      org-src-fontify-natively t
+      org-src-tab-acts-natively t)
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
